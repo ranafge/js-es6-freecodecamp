667b7cb21a1b64aa51979061645b844d73e444c6
ranafge <ranafge@gmail.com>
2021-06-11 12:12:01 +0600

    template leteral

---
+++ es6.js
@@ -0,0 +1,11 @@
+// Template literal
+
+let firstName = 'Rana';
+let lastName = 'fge';
+
+let fullName = `${firstName } ${lastName}`
+
+console.log(fullName) // Rana fge
+
+let fullName2 = `${firstName + lastName}` //Ranafge
+console.log(fullName2)
