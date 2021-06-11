14fd123fdac9fba7bb1965b87db70a3e1384c284
ranafge <ranafge@gmail.com>
2021-06-11 13:04:21 +0600

    object literal added

--- es6.js
+++ es6.js
@@ -29,3 +29,38 @@ document.getElementById('info1').innerText = nickName //fge
 document.getElementById('info2').innerText = city //Dhaka
 
 document.getElementById('info3').innerText = `${'Name ' +name_ + ' '+ nickName + ' ' + city}`
+
+
+function addressMaker(city, state) {
+    const newAddress = { city, state }
+
+    document.getElementById('address').innerText = newAddress.city
+    return document.getElementById('address1').innerText = newAddress.state
+
+
+}
+
+addressMaker('durgapur', 'Rangpur')
+
+
+
+function addressMakerWithObject(address) {
+    const newAddress = {
+        city: address.city,
+        state: address.state,
+        country: address.country
+    }
+    return newAddress
+}
+
+const objAddress = addressMakerWithObject({ city: 'Dhaka', state: 'Rangpur', country: 'Bangladesh' })
+
+console.log(objAddress)
+console.log(objAddress.city) //Dhaka
+console.log(objAddress.state) //Rangpur
+console.log(objAddress.country) //Bangladesh
+
+
+document.getElementById('city').innerText = objAddress.city
+document.getElementById('state').innerText = objAddress.state
+document.getElementById('country').innerText = objAddress.country
