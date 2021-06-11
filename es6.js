28c67f99ad8a7ba4cd53b05d3882ccbfe5f28df5
ranafge <ranafge@gmail.com>
2021-06-11 12:34:11 +0600

    template literal

--- es6.js
+++ es6.js
@@ -9,3 +9,23 @@ console.log(fullName) // Rana fge
 
 let fullName2 = `${firstName + lastName}` //Ranafge
 console.log(fullName2)
+
+document.getElementById('name1').innerText = fullName
+document.getElementById('name2').innerText = fullName2
+
+
+//Destructuring object
+
+const personalInformation = {
+    name_: 'Rana',
+    nickName: 'fge',
+    city: 'Dhaka',
+}
+
+const { name_, nickName, city } = personalInformation
+
+document.getElementById('info').innerText = name_ //Rana
+document.getElementById('info1').innerText = nickName //fge
+document.getElementById('info2').innerText = city //Dhaka
+
+document.getElementById('info3').innerText = `${'Name ' +name_ + ' '+ nickName + ' ' + city}`
